9f8fd86fbf1c6da2fa78fc7f8169750fb7ffca43
Eric Smith <esmith@illumina.com>
2014-04-25 15:31:29 -0700

    Added fxns makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,41 @@
 
 # This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 # This function computes the inverse of the special "matrix" returned by 
 # makeCacheMatrix(). If the inverse has already been calculated (and the 
 # matrix has not changed), then cacheSolve will retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+# Example usage:
+#> A = matrix(c(1,2,3,4),nrow=2,ncol=2,byrow=TRUE)
+#> mcm <- makeCacheMatrix(A)
+#> cacheSolve(mcm)
+#[,1] [,2]
+#[1,] -2.0  1.0
+#[2,]  1.5 -0.5
+#> cacheSolve(mcm)
+#getting cached data
+#[,1] [,2]
+#[1,] -2.0  1.0
+#[2,]  1.5 -0.5
